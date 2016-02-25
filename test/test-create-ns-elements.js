9ecf136efdca15c7ba98167e08abbc30819117f0
Dana Silver <dsilver1221@gmail.com>
2016-02-25 14:40:21 -0500

    Add createElement test.

--- test/test-create-ns-elements.js
+++ test/test-create-ns-elements.js
@@ -8,24 +8,36 @@ var Backbone = require('..');
 var assert = require('assert');
 
 describe('backbone-namespaced-view', function() {
-  it('should create svg elements', function() {
+  it('should use createElementNS when given an unqualified matching namespace', function() {
     var SVGView = Backbone.NSView.extend({
       tagName: 'svg'
     });
+
     var svg = new SVGView().el;
 
-    assert.notEqual(svg, null);
+    assert.equal(svg.tagName, 'svg');
     assert.equal(svg.namespaceURI, 'http://www.w3.org/2000/svg');
   });
 
-  it('should create svg sub-elements', function() {
+  it('should use createElementNS when given an qualified matching namespace', function() {
     var RectView = Backbone.NSView.extend({
       tagName: 'svg:rect'
     });
 
     var rect = new RectView().el;
 
-    assert.notEqual(rect, null);
+    assert.equal(rect.tagName, 'rect');
     assert.equal(rect.namespaceURI, 'http://www.w3.org/2000/svg');
+  });
+
+  it('should use createElement when a namespace does not match', function() {
+    var SpanView = Backbone.NSView.extend({
+      tagName: 'span'
+    });
+
+    var span = new SpanView().el;
+
+    assert.equal(span.tagName, 'SPAN');
+    assert.equal(span.namespaceURI, 'http://www.w3.org/1999/xhtml');
   });
 });
