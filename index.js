40bd8fd635e48318b21528f0e6e7483c5e4d381b
Dana Silver <dsilver1221@gmail.com>
2016-02-25 14:05:17 -0500

    Fixup exports.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // backbone.nsview
 
-(function (factory) {
-  if (typeof define === 'function' && define.amd)
-    define(['underscore', 'backbone', 'exports'], factory);
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['backbone'], factory);
+  } else if (typeof exports !== 'undefined') {
+    module.exports = factory(require('backbone'));
+  } else {
+    factory(Backbone);
+  }
 
-  else if (typeof exports === 'object')
-    factory(require('underscore'), require('backbone'));
+}(function(Backbone) {
 
-  else
-    factory(_, Backbone);
-
-}(function (_, Backbone) {
-  
   // Namespace qualification from d3.ns
   var namespaces = {
     svg: 'http://www.w3.org/2000/svg',
@@ -21,14 +20,14 @@
     xmlns: 'http://www.w3.org/2000/xmlns'
   };
 
-  var qualify = function (name) {
+  var qualify = function(name) {
     var i = name.indexOf(':'), prefix = name;
     if (i >= 0 && (prefix = name.slice(0, i)) !== "xmlns") name = name.slice(i + 1);
     return namespaces.hasOwnProperty(prefix) ? {space: namespaces[prefix], local: name} : name;
   };
 
   Backbone.NSView = Backbone.View.extend({
-    _createElement: function (tagName) {
+    _createElement: function(tagName) {
       var qualified = qualify(tagName);
 
       return qualified.space
